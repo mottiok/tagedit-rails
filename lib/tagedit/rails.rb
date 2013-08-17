6dea4c76e0110ac9795e51211d6e7e8d0a9b1502
Matthew <mottiokla@gmail.com>
2013-08-17 13:49:43 -0700

    add as is all vendor files

---
+++ lib/tagedit/rails.rb
@@ -0,0 +1,7 @@
+require "tagedit/rails/version"
+
+module Tagedit
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/tagedit/rails/version.rb
@@ -0,0 +1,5 @@
+module Tagedit
+  module Rails
+    VERSION = "0.0.1"
+  end
+end
